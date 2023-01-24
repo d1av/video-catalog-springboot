6964ad5e6c33fd1eb62ec3d30152d99cada5dc0a
d1av <davi4alves@gmail.com>
2023-01-24 19:32:56 -0300

    revert: old spring configs

--- backend/infrastructure/src/main/java/com/catalog/infrastructure/Main.java
+++ backend/infrastructure/src/main/java/com/catalog/infrastructure/Main.java
@@ -1,6 +1,5 @@
 package com.catalog.infrastructure;
 
-import com.catalog.application.UseCase;
 import com.catalog.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(WebServerConfig.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
--- backend/infrastructure/src/main/java/com/catalog/infrastructure/configuration/WebServerConfig.java
+++ backend/infrastructure/src/main/java/com/catalog/infrastructure/configuration/WebServerConfig.java
@@ -1,10 +1,9 @@
 package com.catalog.infrastructure.configuration;
 
-import org.hibernate.annotations.Cache;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.catalog.infrastructure")
+@ComponentScan("com.catalog")
 public class WebServerConfig {
 }
