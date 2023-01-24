1fd0da54433285b1233aa9223d8fb9add4e47a32
d1av <davi4alves@gmail.com>
2023-01-24 01:29:26 -0300

    chore: add spring

--- backend/infrastructure/src/main/java/com/catalog/infrastructure/Main.java
+++ backend/infrastructure/src/main/java/com/catalog/infrastructure/Main.java
@@ -1,10 +1,13 @@
 package com.catalog.infrastructure;
 
 import com.catalog.application.UseCase;
+import com.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-      //  System.out.println(new UseCase().execute());
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ backend/infrastructure/src/main/java/com/catalog/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,10 @@
+package com.catalog.infrastructure.configuration;
+
+import org.hibernate.annotations.Cache;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.catalog.infrastructure")
+public class WebServerConfig {
+}
