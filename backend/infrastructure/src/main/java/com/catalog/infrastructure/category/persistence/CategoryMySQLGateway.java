9aa5d9a88ff593408426af2f73c6a44c29198190
d1av <davi4alves@gmail.com>
2023-01-25 13:48:21 -0300

    feat: add update category repository

--- backend/infrastructure/src/main/java/com/catalog/infrastructure/category/persistence/CategoryMySQLGateway.java
+++ backend/infrastructure/src/main/java/com/catalog/infrastructure/category/persistence/CategoryMySQLGateway.java
@@ -21,8 +21,9 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Category create(Category aCategory) {
-        return this.repository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
+        return save(aCategory);
     }
+
 
     @Override
     public void deleteById(CategoryID anId) {
@@ -35,12 +36,16 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public Category update(Category aCategory) {
-        return null;
+    public Category update(final Category aCategory) {
+        return save(aCategory);
     }
 
     @Override
     public Pagination<Category> findAll(CategorySearchQuery aQuery) {
         return null;
+    }
+
+    private Category save(final Category aCategory) {
+        return this.repository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
     }
 }
