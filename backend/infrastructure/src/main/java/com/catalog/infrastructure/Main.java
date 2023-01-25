3aa082cf4064fb897b2ecace13c634874c3faa8f
d1av <davi4alves@gmail.com>
2023-01-25 00:36:54 -0300

    feat: add flyway migrate

--- backend/infrastructure/src/main/java/com/catalog/infrastructure/Main.java
+++ backend/infrastructure/src/main/java/com/catalog/infrastructure/Main.java
@@ -8,8 +8,8 @@ import org.springframework.core.env.AbstractEnvironment;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test");
-        // System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"development");
+        //System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test");
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
