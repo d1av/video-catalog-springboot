4a2cee2a2ef7d9774815ec6b205df6b122c8258e
d1av <davi4alves@gmail.com>
2023-01-28 15:45:14 -0300

    test: add domain genre tests

---
+++ backend/domain/src/test/java/com/catalog/domain/genre/GenreTest.java
@@ -0,0 +1,87 @@
+package com.catalog.domain.genre;
+
+import com.catalog.domain.exceptions.DomainException;
+import com.catalog.domain.validation.handler.ThrowsValidationHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GenreTest {
+
+    @Test
+    public void givenValidParams_whenCallsNewGenre_shouldInstantiateAGente() {
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+
+
+        final var actualGenre = Genre.newGente(expectedName, expectedIsActive);
+
+        Assertions.assertNotNull(actualGenre);
+        Assertions.assertNotNull(actualGenre.getId());
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertEquals(expectedCategories, actualGenre.getCategories().size);
+        Assertions.assertNotNull(actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    public void givenInvalidNullName_whenCallsNewGenre_shouldReceiceAError() {
+        final String expectedName = null;
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualGenre = Genre.newGente(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidEmptyName_whenCallsNewGenre_shouldReceiceAError() {
+        final String expectedName = " ";
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualGenre = Genre.newGente(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenInvalidNameWithLengthGreaterThan255_whenCallsNewGenre_shouldReceiceAError() {
+        final String expectedName = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of " +
+                "classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney " +
+                "College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through " +
+                "the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of" +
+                " \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, " +
+                "very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.";
+        final var expectedIsActive = true;
+        final var expectedCategories = 0;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 1 and 255 characters";
+
+        final var actualGenre = Genre.newGente(expectedName, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> {
+            actualGenre.validate(new ThrowsValidationHandler());
+        });
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+}
