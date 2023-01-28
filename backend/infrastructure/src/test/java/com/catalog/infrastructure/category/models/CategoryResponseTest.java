fb85e8f120d52ce7ffaed7b0ee23b5bd18c2ef41
d1av <davi4alves@gmail.com>
2023-01-28 00:08:53 -0300

    test: jackson test for json format

--- backend/infrastructure/src/test/java/com/catalog/infrastructure/category/models/CategoryResponseTest.java
+++ backend/infrastructure/src/test/java/com/catalog/infrastructure/category/models/CategoryResponseTest.java
@@ -1,12 +1,94 @@
 package com.catalog.infrastructure.category.models;
 
 import com.catalog.JacksonTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.json.JacksonTester;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+import java.time.Instant;
+
 
 @JacksonTest
 public class CategoryResponseTest {
     @Autowired
     private JacksonTester<CategoryResponse> json;
+
+    @Test
+    public void testMarshall() throws IOException {
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedUpdatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var response = new CategoryResponse(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive,
+                expectedCreatedAt,
+                expectedUpdatedAt,
+                expectedDeletedAt
+        );
+
+        final var actualJson = this.json.write(response);
+
+        assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.description", expectedDescription)
+                .hasJsonPathValue("$.is_active", expectedIsActive)
+                .hasJsonPathValue("$.created_at", expectedCreatedAt)
+                .hasJsonPathValue("$.deleted_at", expectedDeletedAt)
+                .hasJsonPathValue("$.updated_at", expectedUpdatedAt);
+    }
+
+    @Test
+    public void testUnMarshall() throws IOException {
+        final var expectedId = "123";
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+        final var expectedCreatedAt = Instant.now();
+        final var expectedUpdatedAt = Instant.now();
+        final var expectedDeletedAt = Instant.now();
+
+        final var json = """
+                {
+                "id": "%s",
+                "name": "%s",
+                "description": "%s",
+                "is_active": "%s",
+                "created_at": "%s",
+                "deleted_at": "%s",
+                "updated_at": "%s"
+                }
+                """.formatted(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedIsActive,
+                expectedCreatedAt.toString(),
+                expectedDeletedAt.toString(),
+                expectedUpdatedAt.toString()
+        );
+
+        final var actualJson = this.json.parse(json);
+
+        assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("id", expectedId)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive)
+                .hasFieldOrPropertyWithValue("createdAt", expectedCreatedAt)
+                .hasFieldOrPropertyWithValue("deletedAt", expectedDeletedAt)
+                .hasFieldOrPropertyWithValue("updatedAt", expectedUpdatedAt);
+    }
 
 }
