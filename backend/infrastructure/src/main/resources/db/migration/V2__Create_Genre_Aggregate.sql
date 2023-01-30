1044396da7edfc17c4d2e0c480661e2820d42a84
d1av <davi4alves@gmail.com>
2023-01-30 12:43:15 -0300

    feat: flyway migration

--- backend/infrastructure/src/main/resources/db/migration/V2__Create_Genre_Aggregate.sql
+++ backend/infrastructure/src/main/resources/db/migration/V2__Create_Genre_Aggregate.sql
@@ -4,14 +4,13 @@ CREATE TABLE genres (
         active BOOLEAN NOT NULL DEFAULT TRUE,
         created_At DATETIME(6) NOT NULL,
         updated_At DATETIME(6) NOT NULL,
-        deleted_At DATETIME(6) NOT NULL,
+        deleted_At DATETIME(6) NOT NULL
 );
 
 CREATE TABLE genres_categories (
         genres_id VARCHAR(36) NOT NULL,
         category_id VARCHAR(36) NOT NULL,
-        CONSTRAINT idx_genre_category UNIQUE(genres_id, category_id),
-        CONSTRAINT fk_genre_id UNIQUE(genres_id) REFERENCES genres (id) ON DELETE CASCADE,
-        CONSTRAINT fk_category_id UNIQUE(category_id) REFERENCES category (id) ON DELETE CASCADE,
-
+        CONSTRAINT idx_genre_category UNIQUE (genres_id, category_id),
+        CONSTRAINT fk_genre_id FOREIGN KEY (genres_id) REFERENCES genres (id) ON DELETE CASCADE,
+        CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
 );
