e9741e48177f696ac55f2c5ebc45fbc979d4b2a9
d1av <davi4alves@gmail.com>
2023-02-01 00:30:51 -0300

    test: end 2 end test of genre

---
+++ backend/infrastructure/src/test/java/com/catalog/e2e/genre/GenreE2ETest.java
@@ -0,0 +1,136 @@
+package com.catalog.e2e.genre;
+
+import com.catalog.ControllerTest;
+import com.catalog.E2ETest;
+import com.catalog.domain.category.CategoryID;
+import com.catalog.domain.genre.GenreID;
+import com.catalog.infrastructure.category.models.CreateCategoryRequest;
+import com.catalog.infrastructure.configuration.json.Json;
+import com.catalog.infrastructure.genre.models.CreateGenreRequest;
+import com.catalog.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@E2ETest
+@Testcontainers
+public class GenreE2ETest {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Container
+    private static final MySQLContainer MY_SQL_CONTAINER =
+            new MySQLContainer("mysql:latest")
+                    .withPassword("root")
+                    .withUsername("root")
+                    .withDatabaseName("adm_videos");
+
+    @DynamicPropertySource
+    public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("mysql.port", () -> MY_SQL_CONTAINER.getMappedPort(3306));
+    }
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToCreateANewGenreWithValidValues() throws Exception {
+        Assertions.assertTrue(MY_SQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, genreRepository.count());
+
+        final var filmes = givenACategory("Filmes", "O melhor filme", true);
+
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of(filmes);
+
+        final var actualId = givenAGenre(expectedName, expectedIsActive, expectedCategories);
+
+        final var actualGenre = genreRepository.findById(actualId.getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertTrue(expectedCategories.size() == actualGenre.getCategoriesIDs().size()
+                && expectedCategories.containsAll(actualGenre.getCategoriesIDs())
+        );
+        Assertions.assertNotNull(actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull(actualGenre.getDeletedAt());
+    }
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToCreateANewGenreWithCategories() throws Exception {
+        Assertions.assertTrue(MY_SQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, genreRepository.count());
+
+
+        final var expectedName = "Ação";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.<CategoryID>of();
+
+        final var actualId = givenAGenre(expectedName, expectedIsActive, expectedCategories);
+
+        final var actualGenre = genreRepository.findById(actualId.getValue()).get();
+
+        Assertions.assertEquals(expectedName, actualGenre.getName());
+        Assertions.assertEquals(expectedIsActive, actualGenre.isActive());
+        Assertions.assertTrue(expectedCategories.size() == actualGenre.getCategoriesIDs().size()
+                && expectedCategories.containsAll(actualGenre.getCategoriesIDs())
+        );
+        Assertions.assertNotNull(actualGenre.getCreatedAt());
+        Assertions.assertNotNull(actualGenre.getUpdatedAt());
+        Assertions.assertNull(actualGenre.getDeletedAt());
+    }
+
+    private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
+        final var aRequestBody = new CreateCategoryRequest(aName, aDescription, isActive);
+
+        final var aRequest = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(aRequestBody));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/categories/", "");
+
+        return CategoryID.from(actualId);
+    }
+
+
+    private GenreID givenAGenre(final String aName, final boolean isActive, final List<CategoryID> categories) throws Exception {
+        final var aRequestBody = new CreateGenreRequest(aName, mapTo(categories, CategoryID::getValue), isActive);
+
+        final var aRequest = MockMvcRequestBuilders.post("/genres")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(aRequestBody));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/genres/", "");
+
+        return GenreID.from(actualId);
+    }
+
+
+    private <A, D> List<D> mapTo(final List<A> actual, final Function<A, D> mapper) {
+        return actual.stream()
+                .map(mapper)
+                .toList();
+    }
+}
