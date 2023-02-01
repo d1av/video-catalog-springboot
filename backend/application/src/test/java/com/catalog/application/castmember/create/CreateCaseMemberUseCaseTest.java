69579d229ee45d5b9d71907502fb33239de2d878
d1av <davi4alves@gmail.com>
2023-02-01 17:29:31 -0300

    test: cast member use case tdd

---
+++ backend/application/src/test/java/com/catalog/application/castmember/create/CreateCaseMemberUseCaseTest.java
@@ -0,0 +1,61 @@
+package com.catalog.application.castmember.create;
+
+import com.catalog.application.Fixture;
+import com.catalog.application.UseCaseTest;
+import com.catalog.domain.castmember.CastMemberGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class CreateCaseMemberUseCaseTest extends UseCaseTest {
+    @InjectMocks
+    private DefaultCreateCastMemberUseCase useCase;
+    @Mock
+    private CastMemberGateway castMemberGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(castMemberGateway);
+    }
+
+    @Test
+    public void givenAValidCommand_whenCreateCastMember_shouldReturnIt() {
+        // given
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var aCommand = CreateCastMemberCommand.with(
+                expectedName,
+                expectedType
+        );
+
+        when(castMemberGateway).create(any())
+                .thenAnswer(returnsFirstArg());
+        // when
+        final var actualOutput = useCase.execute(aCommand);
+
+        // then
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        verify(castMemberGateway).create(argThat(aMember ->
+                Objects.nonNull(aMember.getId())
+                        && Objects.equals(expectedName, aMember.getName())
+                        && Objects.equals(expectedType, aMember.getType())
+                        && Objects.nonNull(aMember.getCreatedAt())
+                        && Objects.nonNull(aMember.getUpdatedAt())
+        ));
+    }
+}
