fb32f4e769629e0deea4df46155dfffffa00e075
d1av <davi4alves@gmail.com>
2023-02-02 11:42:28 -0300

    feat: add cast member jpa entity

---
+++ backend/infrastructure/src/main/java/com/catalog/infrastructure/castmember/persistence/CastMemberJpaEntity.java
@@ -0,0 +1,100 @@
+package com.catalog.infrastructure.castmember.persistence;
+
+import com.catalog.domain.castmember.CastMember;
+import com.catalog.domain.castmember.CastMemberID;
+import com.catalog.domain.castmember.CastMemberType;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity(name = "CastMember")
+@Table(name = "cast_members")
+public class CastMemberJpaEntity {
+    @Id
+    private String id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private CastMemberType type;
+    @Column(name = "created_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant createdAt;
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant updatedAt;
+
+    public CastMemberJpaEntity() {
+    }
+
+    public CastMemberJpaEntity(
+            final String id,
+            final String name,
+            final CastMemberType type,
+            final Instant createdAt,
+            final Instant updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static CastMemberJpaEntity from(final CastMember aMember) {
+        return new CastMemberJpaEntity(
+                aMember.getId().getValue(),
+                aMember.getName(),
+                aMember.getType(),
+                aMember.getCreatedAt(),
+                aMember.getUpdatedAt()
+        );
+    }
+
+    public CastMember toAggregate() {
+        return CastMember.with(
+                CastMemberID.from(getId()),
+                getName(),
+                getType(),
+                getCreatedAt(),
+                getUpdatedAt()
+        );
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public CastMemberType getType() {
+        return type;
+    }
+
+    public void setType(final CastMemberType type) {
+        this.type = type;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(final Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(final Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
