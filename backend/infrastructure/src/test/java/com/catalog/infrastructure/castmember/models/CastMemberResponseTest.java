929771c7fdef02954c25db63bd11af50259c3b63
d1av <davi4alves@gmail.com>
2023-02-04 15:31:23 -0300

    feat: serialize test for cast member response

---
+++ backend/infrastructure/src/test/java/com/catalog/infrastructure/castmember/models/CastMemberResponseTest.java
@@ -0,0 +1,45 @@
+package com.catalog.infrastructure.castmember.models;
+
+import com.catalog.Fixture;
+import com.catalog.JacksonTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JacksonTest
+public class CastMemberResponseTest {
+
+    @Autowired
+    private JacksonTester<CastMemberResponse> json;
+
+    @Test
+    public void testMarshall() throws Exception {
+        final var expectedId = "123";
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type().name();
+        final var expectedCreatedAt = Instant.now().toString();
+        final var expectedUpdatedAt = Instant.now().toString();
+
+        final var response = new CastMemberResponse(
+                expectedId,
+                expectedName,
+                expectedType,
+                expectedCreatedAt,
+                expectedUpdatedAt
+        );
+
+        final var actualJson = this.json.write(response);
+
+        assertThat(actualJson)
+                .hasJsonPathValue("$.id", expectedId)
+                .hasJsonPathValue("$.name", expectedName)
+                .hasJsonPathValue("$.type", expectedType)
+                .hasJsonPathValue("$.createdAt", expectedCreatedAt)
+                .hasJsonPathValue("$.updatedAt", expectedUpdatedAt);
+    }
+
+}
