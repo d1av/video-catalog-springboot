20bc466a948d3da7c121856630f4f2d7c25c1698
d1av <davi4alves@gmail.com>
2023-02-05 14:00:33 -0300

    test: add audio video media instantiation

--- backend/domain/src/main/java/com/catalog/domain/video/AudioVideoMedia.java
+++ backend/domain/src/main/java/com/catalog/domain/video/AudioVideoMedia.java
@@ -8,18 +8,18 @@ public class AudioVideoMedia extends ValueObject {
     private final String checksum;
     private final String name;
     private final String rawLocation;
-    private final String createdLocation;
+    private final String encodedLocation;
     private final MediaStatus status;
 
     public AudioVideoMedia(final String checksum,
                            final String name,
                            final String rawLocation,
-                           final String createdLocation,
+                           final String encodedLocation,
                            final MediaStatus status) {
         this.checksum = Objects.requireNonNull(checksum);
         this.name = Objects.requireNonNull(name);
         this.rawLocation = Objects.requireNonNull(rawLocation);
-        this.createdLocation = Objects.requireNonNull(createdLocation);
+        this.encodedLocation = Objects.requireNonNull(encodedLocation);
         this.status = Objects.requireNonNull(status);
     }
 
@@ -27,10 +27,10 @@ public class AudioVideoMedia extends ValueObject {
             final String checksum,
             final String name,
             final String rawLocation,
-            final String cratedLocation,
+            final String encodedLocation,
             final MediaStatus status
-    ){
-        return new AudioVideoMedia(checksum, name, rawLocation, cratedLocation, status);
+    ) {
+        return new AudioVideoMedia(checksum, name, rawLocation, encodedLocation, status);
     }
 
     public String checksum() {
@@ -45,8 +45,8 @@ public class AudioVideoMedia extends ValueObject {
         return rawLocation;
     }
 
-    public String createdLocation() {
-        return createdLocation;
+    public String encodedLocation() {
+        return encodedLocation;
     }
 
     public MediaStatus status() {
--- backend/domain/src/test/java/com/catalog/domain/video/AudioVideoMediaTest.java
+++ backend/domain/src/test/java/com/catalog/domain/video/AudioVideoMediaTest.java
@@ -1,4 +1,72 @@
 package com.catalog.domain.video;
 
+import com.catalog.domain.Fixture;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AudioVideoMediaTest {
+    @Test
+    public void givenValidParams_whenCallsNewAudioVideo_shouldReturnInstance() {
+        // given
+        final var expectedChecksum = "abc";
+        final var expectedName = Fixture.firstName() + ".png";
+        final var expectedLocation = "/images/ac";
+        final var expectedEncodedLocation = "/images/ac-encoded";
+        final var expectedStatus = MediaStatus.PENDING;
+
+        // when
+        final var actualVideo =
+                AudioVideoMedia.with(
+                        expectedChecksum,
+                        expectedName,
+                        expectedLocation,
+                        expectedEncodedLocation,
+                        expectedStatus
+                );
+
+        // then
+        Assertions.assertNotNull(actualVideo);
+        Assertions.assertEquals(expectedChecksum, actualVideo.checksum());
+        Assertions.assertEquals(expectedName, actualVideo.name());
+        Assertions.assertEquals(expectedLocation, actualVideo.rawLocation());
+        Assertions.assertEquals(expectedEncodedLocation, actualVideo.encodedLocation());
+        Assertions.assertEquals(expectedStatus, actualVideo.status());
+    }
+
+    @Test
+    public void givenTwoVideosWithSameChecksumAndLocation_whenCallsEqualsAudioVideoImage_shouldReturnTrue() {
+        // given
+        final var expectedChecksum = "abc";
+        final var exectedRawLocation = "images/" + Fixture.firstName() + ".png";
+
+        final var img1 = AudioVideoMedia.with(expectedChecksum, Fixture.name(), "", "", MediaStatus.PENDING);
+        final var img2 = AudioVideoMedia.with(expectedChecksum, Fixture.name(), "", "", MediaStatus.PENDING);
+
+        // then
+        Assertions.assertEquals(img1, img2);
+        Assertions.assertNotSame(img1, img2);
+    }
+
+    @Test
+    public void givenITwoVideos_whenCallsWith_shouldReturnError() {
+        Assertions.assertThrows(NullPointerException.class,
+                () -> AudioVideoMedia.with(null, Fixture.name(), "/videos", "/videos", MediaStatus.PENDING)
+        );
+
+        Assertions.assertThrows(NullPointerException.class,
+                () -> AudioVideoMedia.with("abc", null, "/videos", "/videos", MediaStatus.PENDING)
+        );
+
+        Assertions.assertThrows(NullPointerException.class,
+                () -> AudioVideoMedia.with("abc", Fixture.name(), null, "/videos", MediaStatus.PENDING)
+        );
+
+        Assertions.assertThrows(NullPointerException.class,
+                () -> AudioVideoMedia.with("abc", Fixture.name(), "/videos", null, MediaStatus.PENDING)
+        );
+
+        Assertions.assertThrows(NullPointerException.class,
+                () -> AudioVideoMedia.with("abc", Fixture.name(), "/videos", "/videos", null)
+        );
+    }
 }
